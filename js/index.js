db799a90173a916bf437a8d0df170c02e4b72d10
BURHANUDDIN <Bmerchant945@gmail.com>
2020-12-24 01:04:14 +0530

    changed style of the output window and removed the styling for the button

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 const editorInput = document.getElementById("editor");
 const editorOutput = document.getElementById("output");
 const displayOutput = () => {
-  console.log("here");
   const HTML = editorInput.value;
   editorOutput.innerHTML = HTML;
 };
